9aac539dbcc4877ad0ca96a3c007e1647059821c
William Hicks <whicks@nvidia.com>
2022-08-09 13:56:16 -0400

    Apply existing patches

--- cpp/cmake/thirdparty/get_rmm.cmake
+++ cpp/cmake/thirdparty/get_rmm.cmake
@@ -17,7 +17,8 @@
 function(find_and_configure_rmm)
     include(${rapids-cmake-dir}/cpm/rmm.cmake)
     rapids_cpm_rmm(BUILD_EXPORT_SET raft-exports
-                   INSTALL_EXPORT_SET  raft-exports)
+                   INSTALL_EXPORT_SET  raft-exports
+                   FIND_PACKAGE_ARGUMENTS REQUIRED)
 endfunction()
 
 find_and_configure_rmm()
